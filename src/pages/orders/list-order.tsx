aa6faf948fcfb89d5267e5021640262f0bf8e3d1
Bich Phuong <bichphuong13042002@gmail.com>
2024-01-11 15:42:12 +0700

    List Orders: Create Card list orders

--- src/pages/orders/list-order.tsx
+++ src/pages/orders/list-order.tsx
@@ -2,12 +2,14 @@ import { ListRenderer } from "components/list-renderer";
 import { ProductItem } from "components/product/item";
 import React, { FC, Suspense } from "react";
 import { useRecoilValue } from "recoil";
+import "./orders.css";
 import {
   categoriesState,
   productsByCategoryState,
   selectedCategoryIdState,
 } from "state";
-import { Box, Header, Icon, Page, Tabs, Text, useNavigate } from "zmp-ui";
+import { Card } from "zmp-react";
+import { Box, Header, Page, Tabs, Text, useNavigate } from "zmp-ui";
 
 const HistoryPicker: FC = () => {
   const categories = useRecoilValue(categoriesState);
@@ -31,18 +33,48 @@ const HistoryPicker: FC = () => {
             items={[
               {
                 navigate: "/order-detail",
-                left: <Icon icon="zi-user" />,
+                // left: <Icon icon="zi-user" />,
                 right: (
                   <Box flex>
-                    <Text.Header className="flex-1 items-center font-normal">
-                      Bấm vô đây để vào chi tiết đơn hàng nào
-                    </Text.Header>
-                    <Icon icon="zi-chevron-right" />
+                    <Card inset>
+                      <div className="flex justify-between time-order  mb-2">
+                        <Text className="text-[17px]">10/12/2024, 08:23</Text>
+                        <Text className="font-bold bg-emerald-100 p-0.5 pr-1 pl-1 rounded-md text-green">
+                          Giao thành công
+                        </Text>
+                      </div>
+                      <div className="flex mb-2">
+                        <div className="m-2">
+                          <img
+                            className="img-orders rounded-md"
+                            src="https://www.cnet.com/a/img/resize/36e8e8fe542ad9af413eb03f3fbd1d0e2322f0b2/hub/2023/02/03/afedd3ee-671d-4189-bf39-4f312248fb27/gettyimages-1042132904.jpg?auto=webp&fit=crop&height=1200&width=1200"
+                          />
+                        </div>
+                        <div>
+                          <Text.Header className="text-[18px] leading-6 mb-2">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit.
+                          </Text.Header>
+                          <Text className="text-[17px]">
+                            2 phần - <b className="font-semibold">132.000đ</b>
+                          </Text>
+                        </div>
+                      </div>
+                      <hr className="hr-order" />
+                      <div className="flex mt-4 justify-center">
+                        <Text.Header className="flex-1 align-middle font-normal text-sm">
+                          Xem chi tiết đơn hàng
+                        </Text.Header>
+                        <button className="font-bold bg-sky-200 p-1 pl-6 pr-6 rounded-md text-cyan-800 text-sm hover:text-sky-200 hover:bg-cyan-800">
+                          Đặt lại
+                        </button>
+                      </div>
+                    </Card>
                   </Box>
                 ),
               },
             ]}
-            renderLeft={(item) => item.left}
+            // renderLeft={(item) => item.left}
             renderRight={(item) => item.right}
           />
         </Suspense>
