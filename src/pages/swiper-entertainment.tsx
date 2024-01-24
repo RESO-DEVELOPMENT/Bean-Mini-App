f5b5c4968e78ba07110eff828fa98e4b306dab65
phuongvtb <bichphuong13042002@gmail.com>
2024-01-24 22:53:30 +0700

    fix[Home] Fix image center & next Button slider

--- src/pages/swiper-entertainment.tsx
+++ src/pages/swiper-entertainment.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 import { useNavigate } from "react-router";
 import { useRecoilValueLoadable } from "recoil";
 import { listBlogState } from "state";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import { Box } from "zmp-ui";
 
@@ -12,12 +12,31 @@ const container2Style: React.CSSProperties = {
   backgroundColor: "#ffffff",
   borderRadius: "10px",
   boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-  margin: "10px",
-  height: "140px",
-  width: "140px",
+  margin: "auto",
+  height: "150px",
+  width: "150px",
+};
+
+const NextButton = () => {
+  const swiper = useSwiper();
+  return (
+    <button onClick={() => swiper.slideNext()}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="25"
+        fill="#04bfad"
+        className="bi bi-arrow-right-circle-fill"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+      </svg>
+    </button>
+  );
 };
 
 export const SwiperEn: FC = () => {
+  const swiper = useSwiper();
   const blogList = useRecoilValueLoadable(listBlogState);
   const navigate = useNavigate();
   const gotoPage = (id: string) => {
@@ -30,6 +49,7 @@ export const SwiperEn: FC = () => {
           <SwiperSlide
             key={`slide${index + 1}`}
             onClick={() => gotoPage(item.id)}
+            className="p-1"
           >
             <div style={container2Style}>
               <img
@@ -44,13 +64,16 @@ export const SwiperEn: FC = () => {
               />
             </div>
             <div
-              className="slide-label mr-1 text-sm"
+              className="slide-label m-3 text-sm font-medium"
               style={{ textAlign: "center" }}
             >
               {item.title}
             </div>
           </SwiperSlide>
         ))}
+        <div className="absolute top-1/3 right-0 transform -translate-y-1/2 z-10">
+          <NextButton />
+        </div>
       </Swiper>
     </Box>
   ) : (
