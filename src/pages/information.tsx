a0c6ab5ea4dbfefff7df38a2f17d62b00836a612
khang2905 <tvkhang2905@gmail.com>
2024-01-26 00:17:57 +0700

    fix UI InFoPage
    có thêm popup trong giỏ hàng

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -1,118 +1,47 @@
-import React, { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { Icon } from "zmp-ui";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValueLoadable } from "recoil";
+import { userState } from "state";
+import logo from "static/logo.png";
 
 const InformationPage = () => {
   const navigate = useNavigate();
-  const { avatarUrl, name, birthDate, gender, email } = useParams();
-  const [isEditingAll, setIsEditingAll] = useState(false);
-  const [editableInfo, setEditableInfo] = useState({
-    name: "Tên của bạn",
-    birthDate: "dd/mm/yyyy",
-    gender: "Giới tính",
-    email: "email@example.com",
-  });
+  const user = useRecoilValueLoadable(userState);
 
-  const handleEditAllClick = () => {
-    setIsEditingAll(true);
-  };
-
-  const handleSaveAllClick = () => {
-    setIsEditingAll(false);
-  };
-
-  const handleChange = (field, value) => {
-    setEditableInfo({ ...editableInfo, [field]: value });
-  };
-  const formatDate = (dateString) => {
-    if (dateString === "dd/mm/yyyy" || !dateString.includes("-")) {
-      return dateString;
-    }
-    const [year, month, day] = dateString.split("-");
-    return `${day}/${month}/${year}`;
-  };
-
-  const handleCameraClick = () => {};
   const handleBackClick = () => {
     navigate(-1);
   };
+
   return (
-    <div
-      className="flex-1 scrollable-container"
-      style={{ background: "#14D9C5" }}
-    >
-      <button className="back-button" onClick={handleBackClick}>
-        <Icon icon="zi-chevron-left-header" />
-        Back
-      </button>
-      <div className="info-box">
-        <div className="avatar-wrapper">
-          <img className="avatar" src={avatarUrl} alt="Avatar" />
-          <button className="camera-icon" onClick={handleCameraClick}>
-            <Icon icon="zi-camera" />
-          </button>
+    <div className="flex-1 scrollable-container">
+      <div className="information-page">
+        <div className="avatar-container">
+          <img
+            src={user.contents.avatar || logo}
+            alt="User Avatar"
+            className="avatar"
+          />
         </div>
-        <h2>Thông tin về bạn</h2>
-
-        {isEditingAll ? (
-          <div>
+        <div className="user-details">
+          <div className="user-detail-row">
+            <div className="label text-l ">Tên</div>
             <input
-              type="text"
-              value={editableInfo.name}
-              onChange={(e) => handleChange("name", e.target.value)}
-            />
-            <input
-              type="date"
-              value={
-                isEditingAll && editableInfo.birthDate !== "dd/mm/yyyy"
-                  ? editableInfo.birthDate
-                  : ""
-              }
-              placeholder="dd/mm/yyyy"
-              onChange={(e) => handleChange("birthDate", e.target.value)}
-            />
-
-            <select
-              value={editableInfo.gender}
-              onChange={(e) => handleChange("gender", e.target.value)}
-            >
-              <option value="Nam">Nam</option>
-              <option value="Nữ">Nữ</option>
-              <option value="Khác">Khác</option>
-            </select>
-            <input
-              type="email"
-              value={editableInfo.email}
-              onChange={(e) => handleChange("email", e.target.value)}
+              className="input-field font-bold"
+              value={user.contents.name}
+              readOnly
+              disabled
             />
           </div>
-        ) : (
-          <div>
-            <p>
-              <strong>Tên:</strong> {editableInfo.name}
-            </p>
-            <p>
-              <strong>Ngày sinh:</strong> {formatDate(editableInfo.birthDate)}
-            </p>
-            <p>
-              <strong>Giới tính:</strong> {editableInfo.gender}
-            </p>
-            <p>
-              <strong>Email:</strong>{" "}
-              <a href={`mailto:${editableInfo.email}`}>{editableInfo.email}</a>
-            </p>
+          <div className="user-detail-row">
+            <div className="label text-l ">Số điện thoại:</div>
+            <input
+              className="input-field font-bold"
+              value={user.contents.phone || "Số điện thoại không có"}
+              readOnly
+              disabled
+            />
           </div>
-        )}
-
-        {isEditingAll ? (
-          <button className="edit-button" onClick={handleSaveAllClick}>
-            Lưu{" "}
-          </button>
-        ) : (
-          <button className="edit-button" onClick={handleEditAllClick}>
-            Chỉnh Sửa{" "}
-          </button>
-        )}
+        </div>
       </div>
     </div>
   );
