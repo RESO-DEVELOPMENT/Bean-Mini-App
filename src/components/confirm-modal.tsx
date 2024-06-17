5f5bbba5667cdf6a6e3adde5f645772272b27b7f
NguyenDat0809 <datngx.dev@gmail.com>
2024-06-17 13:52:40 +0700

    refactor: chỉnh sửa css

--- src/components/confirm-modal.tsx
+++ src/components/confirm-modal.tsx
@@ -1,5 +1,5 @@
 import React, { FC, SetStateAction } from "react";
-import { Modal } from "zmp-ui";
+import { Icon, Modal } from "zmp-ui";
 
 interface ConfirmModalProps {
   handleYes?: any;
@@ -15,8 +15,8 @@ interface ConfirmModalProps {
 const ConfirmModal: FC<ConfirmModalProps> = ({
   handleYes = () => {},
   handleNo = () => {},
-  yesTitle = "Yes",
-  noTitle = "No",
+  yesTitle = "Đồng ý",
+  noTitle = "Hủy",
   description,
   title,
   visible,
@@ -24,27 +24,32 @@ const ConfirmModal: FC<ConfirmModalProps> = ({
 }) => {
   return (
     <Modal
-    className="text-center"
+      className="relative text-center"
       visible={visible}
       title={title}
       onClose={() => {
         setVisible(false);
       }}
-     
+      maskClosable={false}
       actions={[
         {
           text: `${yesTitle}`,
           highLight: true,
           onClick: handleYes,
         },
-        {
-          text: `${noTitle}`,
-          close: true,
-          onClick: handleNo,
-        },
       ]}
       description={description}
-    />
+    >
+      <button
+        className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+        onClick={() => {
+          setVisible(false);
+          handleNo();
+        }}
+      >
+        <Icon icon="zi-close" />
+      </button>
+    </Modal>
   );
 };
 
