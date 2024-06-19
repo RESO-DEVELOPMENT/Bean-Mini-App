a124ff6831efa28cca1b548b5a9714decc5dcf10
NguyenDat0809 <datngx.dev@gmail.com>
2024-06-19 11:44:45 +0700

    fix: sửa lỗi khi có sản phẩm type Single gây sập app

--- src/components/product/picker.tsx
+++ src/components/product/picker.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useEffect, useState } from "react";
+import React, { FC, ReactNode, useCallback, useEffect, useState } from "react";
 import { useRecoilValue, useRecoilState } from "recoil";
 import { childrenProductState } from "states/product.state";
 import { cartState } from "../../states/cart.state";
@@ -20,6 +20,8 @@ export const ProductPicker: FC<ProductPickerProps> = ({
   // const member = useRecoilValueLoadable(memberState);
   const [cart, setCart] = useRecoilState(cartState);
 
+
+  
   const childProductsInMenu = useRecoilValue(childrenProductState);
 
   let currentChildOfProduct = childProductsInMenu
@@ -31,11 +33,11 @@ export const ProductPicker: FC<ProductPickerProps> = ({
     )
     .sort((a, b) => a.sellingPrice - b.sellingPrice);
   //lưu lại các sản phẩm con
-  const currentChildren = currentChildOfProduct
+  const currentChildren = currentChildOfProduct.length > 0
     ? [...currentChildOfProduct]
     : [product];
   const [productChosen, setProductChosen] = useState<Product>(
-    currentChildren ? currentChildren![0] : ({} as Product)
+    currentChildren.length > 0 ? currentChildren![0] : (product)
   );
   const [visible, setVisible] = useState(false);
   //lưu lại productList nếu đã có
@@ -43,17 +45,64 @@ export const ProductPicker: FC<ProductPickerProps> = ({
     (p) => p.productInMenuId === productChosen.menuProductId
   );
 
-  const changeCartItemNumber = (
-    product: Product | ProductList,
-    quantity: number
-  ) => {
+  // const changeCartItemNumber = (
+  //   product: Product | ProductList,
+  //   quantity: number
+  // ) => {
+  //   setCart((prevCart) => {
+  //     let anotherCart = { ...prevCart };
+  //     if (productInCart != null) {
+  //       let newProductList = anotherCart.productList.map((item) => {
+  //         if (
+  //           item.productInMenuId === (product as ProductList).productInMenuId
+  //         ) {
+  //           return {
+  //             ...item,
+  //             totalAmount: item.sellingPrice * quantity,
+  //             finalAmount: item.sellingPrice * quantity - item.discount,
+  //             quantity: quantity,
+  //           };
+  //         }
+  //         return item;
+  //       });
+  //       // console.log(newProductList);
+  //       let res = {
+  //         ...anotherCart,
+  //         productList: newProductList,
+  //       };
+  //       return prepareCart(res);
+  //     }
+  //     const cartItem: ProductList = {
+  //       productInMenuId: (product as Product)!.menuProductId,
+  //       parentProductId: product!.parentProductId,
+  //       name: product!.name,
+  //       type: product!.type,
+  //       quantity: quantity!,
+  //       sellingPrice: product!.sellingPrice,
+  //       code: product!.code,
+  //       categoryCode: product!.code,
+  //       totalAmount: product!.sellingPrice * quantity!,
+  //       discount: (product as Product).discountPrice,
+  //       finalAmount:
+  //         product!.sellingPrice * quantity! -
+  //         (product as Product)!.discountPrice,
+  //       picUrl: product!.picUrl,
+  //     };
+  //     let res = {
+  //       ...anotherCart,
+  //       productList: cart.productList.concat(cartItem),
+  //     };
+  //     return prepareCart(res);
+  //   });
+  // };
+
+  const changeCartItemNumber = useCallback((product: Product | ProductList, quantity: number) => {
+    console.log(product);
     setCart((prevCart) => {
       let anotherCart = { ...prevCart };
       if (productInCart != null) {
         let newProductList = anotherCart.productList.map((item) => {
-          if (
-            item.productInMenuId === (product as ProductList).productInMenuId
-          ) {
+          if (item.productInMenuId === (product as ProductList).productInMenuId) {
             return {
               ...item,
               totalAmount: item.sellingPrice * quantity,
@@ -63,36 +112,35 @@ export const ProductPicker: FC<ProductPickerProps> = ({
           }
           return item;
         });
-        // console.log(newProductList);
         let res = {
           ...anotherCart,
           productList: newProductList,
         };
         return prepareCart(res);
       }
+
       const cartItem: ProductList = {
-        productInMenuId: (product as Product)!.menuProductId,
-        parentProductId: product!.parentProductId,
-        name: product!.name,
-        type: product!.type,
-        quantity: quantity!,
-        sellingPrice: product!.sellingPrice,
-        code: product!.code,
-        categoryCode: product!.code,
-        totalAmount: product!.sellingPrice * quantity!,
+        productInMenuId: (product as Product).menuProductId,
+        parentProductId: product.parentProductId,
+        name: product.name,
+        type: product.type,
+        quantity,
+        sellingPrice: product.sellingPrice,
+        code: product.code,
+        categoryCode: product.code,
+        totalAmount: product.sellingPrice * quantity,
         discount: (product as Product).discountPrice,
-        finalAmount:
-          product!.sellingPrice * quantity! -
-          (product as Product)!.discountPrice,
-        picUrl: product!.picUrl,
+        finalAmount: product.sellingPrice * quantity - (product as Product).discountPrice,
+        picUrl: product.picUrl,
       };
+
       let res = {
         ...anotherCart,
         productList: cart.productList.concat(cartItem),
       };
       return prepareCart(res);
     });
-  };
+  }, [setCart, cart.productList, productInCart]);
 
   return (
     <>
